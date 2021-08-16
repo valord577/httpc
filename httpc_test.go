c7d3e8525fed643bdb6205f7ed7187c17a2c0e44
valord577 <valord577@gmail.com>
2021-08-16 12:30:41 +0800

    add test coverage

--- httpc_test.go
+++ httpc_test.go
@@ -8,12 +8,48 @@ import (
 
 // @author valor.
 
-func TestHttpc(t *testing.T) {
+func TestHttpc000(t *testing.T) {
 	c := PackedReq{
 		URL:              "https://www.google.com",
 		Method:           http.MethodGet,
 		ReqBodyPublisher: PublisherNoBody{},
 		RespBodyHandler:  RespBodyAsByteArray{},
+	}
+
+	bs, err := c.Send()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", bs)
+}
+
+func TestHttpc001(t *testing.T) {
+	c := PackedReq{
+		URL:    "https://www.google.com",
+		Method: http.MethodPost,
+		ReqBodyPublisher: PublisherRawBytesBody{
+			Body: nil,
+			Type: RawTypeText,
+		},
+		RespBodyHandler: RespBodyAsByteArray{},
+	}
+
+	bs, err := c.Send()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", bs)
+}
+
+func TestHttpc002(t *testing.T) {
+	c := PackedReq{
+		URL:    "https://www.google.com",
+		Method: http.MethodPost,
+		ReqBodyPublisher: PublisherRawStringBody{
+			Body: "",
+			Type: RawTypeText,
+		},
+		RespBodyHandler: RespBodyAsByteArray{},
 	}
 
 	bs, err := c.Send()
